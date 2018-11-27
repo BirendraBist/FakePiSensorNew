24f35beed1da4ade58b378ba827d710aca8fb409
birendra <33861012+bire0011@users.noreply.github.com>
2018-11-27 13:38:13 +0100

    ch

--- FakePiSensor/Program.cs
+++ FakePiSensor/Program.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FakePiSensor
@@ -10,12 +13,45 @@ namespace FakePiSensor
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            UdpClient udpSender = new UdpClient(0) { EnableBroadcast = true };
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Broadcast, 7000);
+            //udpSender.Connect(endPoint);
+            Console.WriteLine("Broadcast ready. Get started Press Enter");
+            Console.ReadLine();
+            while (true)
+            {
+                RandomGenerator _rand = new RandomGenerator();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+               double _healthId = _rand.GetHealthId();
+                double _location = _rand.GetLocation();
+                double _bodyTemperature = _rand.GetBodyTemperature();
+                double _upperbloodPressure = _rand.GetUpperbloodPressure();
+                double _lowerbloodPressure = _rand.GetLowerbloodPressure();
+                double _heartBeat = _rand.GetHeartBeat();
+                int _userId = _rand.GetUserId();
+
+                string sensorData = $"UserId:{_userId}\n HealthId: {_healthId}\n Location:{_location}\n Body Temperature:{_bodyTemperature}\n Upper blood pressure: {_upperbloodPressure}\n Low Blood Pressure:{_lowerbloodPressure}\n Heart Beat:{_healthId}\n userid:{_userId}\n";
+
+                Byte[] sendBytes = Encoding.ASCII.GetBytes(sensorData);
+
+                try
+                {
+                    udpSender.Send(sendBytes, sendBytes.Length, endPoint);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+                Console.WriteLine("UserId:" + _userId);
+                Console.WriteLine("HealthId:" + _healthId);
+                Console.WriteLine("Location:" + _location);
+                Console.WriteLine("Upper blood Pressure:" + _upperbloodPressure);
+                Console.WriteLine("Lowe Blood Pressure:" + _lowerbloodPressure);
+                Console.WriteLine("Heart beat:" + _heartBeat);
+                Console.WriteLine("Location:" + _location);
+                Console.WriteLine("Broadcasting data on Port no: 7000");
+                Thread.Sleep(2000);
+            }
         }
     }
 }
---
+++ FakePiSensor/RandomGenerator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakePiSensor
+{
+     public  class RandomGenerator
+    {
+        private Random _healthIdRandom;
+        private Random _locationRandom;
+        private Random _bodyTemperatureRandom;
+        private Random _upperbloodPressureRandom;
+        private Random _lowerbloodPressureRandom;
+        private Random _heartBeatRandom;
+        private Random _userIdRandom;
+
+        public RandomGenerator()
+        {
+            _healthIdRandom =new Random();
+            _bodyTemperatureRandom = new Random();
+            _locationRandom=new Random();
+            _upperbloodPressureRandom= new Random();
+            _lowerbloodPressureRandom=new Random();
+            _heartBeatRandom=new Random();
+            _userIdRandom=new Random();
+        }
+
+          public double GetHealthId()
+            {
+                return _healthIdRandom.Next(95, 140);
+            }
+        public double GetLocation()
+        {
+            return _locationRandom.Next(95, 140);
+        }
+
+        public double GetBodyTemperature()
+            {
+                return _bodyTemperatureRandom.Next(50, 85);
+            }
+
+        public double GetUpperbloodPressure()
+        {
+            return _upperbloodPressureRandom.Next(70, 130);
+
+        }
+
+        public double GetLowerbloodPressure()
+        {
+            return _lowerbloodPressureRandom.Next(70, 130);
+        }
+        public double GetHeartBeat()
+        {
+            return _heartBeatRandom.Next(70, 100);
+        }
+        public int GetUserId()
+        {
+            return _userIdRandom.Next(1, 100);
+        }
+
+    }
+}
